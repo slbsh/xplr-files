23c0a3145dfb58d81e18d4a8ca2b7757cc97c2fb
slbsh <crafter.antek@gmail.com>
2024-10-11 00:11:06 +0200

    change: plugin handling rewwork

--- init.lua
+++ init.lua
@@ -6,34 +6,14 @@ xplr.config.general.initial_layout = "no_help_no_selection"
 
 -- plugin manager
 local home = os.getenv("HOME")
-local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
-local xpm_url = "https://github.com/dtomvan/xpm.xplr"
+package.path = 
+   home .. "/.config/xplr/plugins/?/init.lua;" .. 
+   home .. "/.config/xplr/plugins/?.lua;" .. 
+   package.path
 
-package.path = package.path
-  .. ";"
-  .. xpm_path
-  .. "/?.lua;"
-  .. xpm_path
-  .. "/?/init.lua"
-
-os.execute(
-  string.format(
-    "[ -e '%s' ] || git clone '%s' '%s'",
-    xpm_path,
-    xpm_url,
-    xpm_path
-  )
-)
-
-
-require("xpm").setup({
-   'dtomvan/xpm.xplr',
-   'sayanarijit/map.xplr',
-   'sayanarijit/find.xplr',
-   'sayanarijit/scp.xplr',
-   'sayanarijit/material-landscape2.xplr',
-   'sayanarijit/xclip.xplr',
-})
+require("material-landscape2").setup()
+require("tree").setup({ as_initial_layout = true })
+require("map").setup({ editor = "nvim", editor_key = "ctrl-e" })
 
 
 table.remove(xplr.config.general.table.row.cols, 5)
@@ -44,18 +24,6 @@ xplr.config.general.table.header.cols = {
    { format = "perm" },
    { format = "size" },
 }
-
-
-package.path = package.path
-  .. ";"
-  .. home
-  .. "/.config/xplr/tree.lua;"
-
-require("tree").setup({
-   as_initial_layout = true,
-})
-
-
 
 
 --
@@ -88,42 +56,6 @@ xplr.config.modes.builtin.default.key_bindings.on_key["`"] = {
           "$XPLR" -m 'FocusPath: %q' "$PTH"
         fi
       ]===],
-    },
-  },
-}
- 
-
---
--- history fuzzy search
-xplr.config.modes.builtin.go_to.key_bindings.on_key.h = {
-  help = "history",
-  messages = {
-    "PopMode",
-    {
-      BashExec0 = [===[
-        PTH=$(cat "${XPLR_PIPE_HISTORY_OUT:?}" | sort -z -u | fzf --read0)
-        if [ "$PTH" ]; then
-          "$XPLR" -m 'ChangeDirectory: %q' "$PTH"
-        fi
-      ]===],
-    },
-  },
-}
-
---
--- batch rename
-xplr.config.modes.builtin.default.key_bindings.on_key.R = {
-  help = "batch rename",
-  messages = {
-    {
-      BashExec = [===[
-       SELECTION=$(cat "${XPLR_PIPE_SELECTION_OUT:?}")
-       NODES=${SELECTION:-$(cat "${XPLR_PIPE_DIRECTORY_NODES_OUT:?}")}
-       if [ "$NODES" ]; then
-         echo -e "$NODES" | renamer
-         "$XPLR" -m ExplorePwdAsync
-       fi
-     ]===],
     },
   },
 }
