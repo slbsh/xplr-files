f27eb31f5c8107f66dc571f86289de6937f3db36
slbsh <crafter.antek@gmail.com>
2025-05-30 15:30:29 +0200

    idk

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-version = "0.21.2"
+version = "1.0.0"
 xplr.config.general.show_hidden = true
 
 xplr.config.general.initial_layout = "no_help_no_selection"
@@ -12,7 +12,10 @@ package.path =
    package.path
 
 require("material-landscape2").setup()
-require("tree").setup({ as_initial_layout = true })
+require("tree").setup({ 
+	as_initial_layout = true,
+	toggle_layout_key = "",
+})
 require("map").setup({ editor = "nvim", editor_key = "ctrl-e" })
 require("find").setup()
 
@@ -26,37 +29,7 @@ xplr.config.general.table.header.cols = {
    { format = "size" },
 }
 
-
---
--- Bookmarks
-xplr.config.modes.builtin.default.key_bindings.on_key.m = {
-  help = "bookmark",
-  messages = {
-    {
-      BashExecSilently0 = [===[
-        PTH="${XPLR_FOCUS_PATH:?}"
-        PTH_ESC=$(printf %q "$PTH")
-        if echo "${PTH:?}" >> "${XPLR_SESSION_PATH:?}/bookmarks"; then
-          "$XPLR" -m 'LogSuccess: %q' "$PTH_ESC added to bookmarks"
-        else
-          "$XPLR" -m 'LogError: %q' "Failed to bookmark $PTH_ESC"
-        fi
-      ]===],
-    },
-  },
-}
-
-xplr.config.modes.builtin.default.key_bindings.on_key["`"] = {
-  help = "go to bookmark",
-  messages = {
-    {
-      BashExec0 = [===[
-        PTH=$(cat "${XPLR_SESSION_PATH:?}/bookmarks" | fzf --no-sort)
-        PTH_ESC=$(printf %q "$PTH")
-        if [ "$PTH" ]; then
-          "$XPLR" -m 'FocusPath: %q' "$PTH"
-        fi
-      ]===],
-    },
-  },
+xplr.config.modes.builtin.default.key_bindings.on_key["esc"] = {
+	help = "quit",
+	messages = { "Quit" },
 }
